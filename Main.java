b3abb134aad39939ab335f2de21defdb93a3b52e
Polina <s25276@pjwstk.edu.pl>
2022-12-24 16:40:51 +0100

    oddaje

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+            String str = "Never/gonna/give/you/up";
+            String sstr[] = str.split("/");
+
+            System.out.println(Arrays.toString(sstr));
+
+
+
+
+
+
+        }
+    }
